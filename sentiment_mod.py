8902b14daa561942be157b0c1fa1db53cfc64d1a
abha-m <abhamutalik@gmail.com>
2018-03-31 16:23:49 +0530

    dataset changed

--- sentiment_mod.py
+++ sentiment_mod.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar 23 20:06:39 2018
-
-@author: Abha Mutalik
-"""
 import nltk
 import pandas as pd
 import re
@@ -18,11 +11,17 @@ sentiment_list = []
 
 class Initialize:
     test_tweets_start = []
+    pat1 = r'@[A-Za-z0-9]+'
+    pat2 = r'https?://[A-Za-z0-9./]+'
+    combined_pat = r'|'.join((pat1, pat2))
 
     def init_test_tweets(self, tweets):
         test_tweets = []
         for (text) in tweets:
-            words_filtered = [e.lower() for e in text.split() if len(e) >= 3]
+            filtered = re.sub(self.combined_pat, '', text)
+            filtered = filtered.replace("'", "")
+            # filtered = re.sub("[^a-zA-Z]", " ", filtered)
+            words_filtered = [e.lower() for e in filtered.split() if len(e) >= 3]
             test_tweets.append((words_filtered))
         # print(test_tweets[1:4])
         return test_tweets
@@ -30,7 +29,9 @@ class Initialize:
     def init_train_tweets(self, tweets):
         train_tweets = []
         for (text, target) in tweets:
-            words_filtered = [e.lower() for e in text.split() if len(e) >= 3]
+            filtered = re.sub(self.combined_pat, '', text)
+            filtered = re.sub("[^a-zA-Z]", " ", filtered)
+            words_filtered = [e.lower() for e in filtered.split() if len(e) >= 3]
             if (target == 0):
                 target = 'negative'
             # elif (target == 2):
@@ -65,65 +66,70 @@ class Initialize:
         inittrain = self.init_train_tweets(tweets)
         init_train.extend(inittrain)
         inittest = self.init_test_tweets(self.test_tweets_start)    #UNCOMMENT THIS
-
         # inittest = [
         #     ['the', 'would', 'safer', 'place', 'thousands', 'guns', 'weren', 'sold', 'every', 'day', 'without',
         #      'background', 'checks'],
         #     ['donald', 'trump', 'takes', 'hard', 'look', 'himself', 'new', 'simpsons', 'sketch'],
         #     ['desperate', 'for', 'victory', 'aussies', 'back', 'cheating', 'suspected', 'ball', 'tampering', 'bancroft',
-        #      'caught', 'tape', 'cau']]  # COMMENT THIS
+        #      'caught', 'tape', 'cau']]
         init_test.extend(inittest)
         # features_train = self.get_words_features(inittrain)
         word_features.extend(self.get_words_features(inittrain))
         # training_set = nltk.classify.apply_features(extract_features(), inittrain)
         # print(training_set[1:4])
-
         # print(self.extract_features(inittrain))
 
 
 def main():
-    file = r'/home/abha/SE2018/amazon_cells_labelled.txt'
-    df1 = pd.read_csv(file, sep='\t',
-                      names=["text", "target"])
+    file = r'/home/abha/SE-Project-Twitzee-datasets/traintweets.csv'
+    df = pd.read_csv(file,encoding='cp1252',names=["target", "id", "date", "flag", "user", "text"])
+    df1 = df[['text','target']]
+    df2 = df1.loc[df1['target'] == 4]
+    df3 = df1.loc[df1['target'] == 0]
+    df2 = df2.sample(frac=1)
+    df2 = df2[0:4000]
+    df3 = df3.sample(frac=1)
+    df3 = df3[0:4000]
+    finaldf = pd.concat([df2,df3])
 
-    tweets = df1.values.tolist()
-
+    tweets = finaldf.sample(frac=1).values.tolist()
     Initialize(tweets)
+    # print(init_train[0:10])
     training_set = nltk.classify.apply_features(Initialize.extract_features, init_train)
     # print(training_set[1:4])
 
     # classifier = nltk.NaiveBayesClassifier.train(training_set)
     #
-    # save_classifier = open("naivebayes.pickle", "wb")
+    # save_classifier = open("testpickles.pickle", "wb")
     # pickle.dump(classifier, save_classifier)
     # save_classifier.close()
 
-    classifier_pkl_opn = open("naivebayes.pickle", "rb")
+    classifier_pkl_opn = open("testpickles.pickle", "rb")
     classifier_pkl = pickle.load(classifier_pkl_opn)
 
-    # print(classifier.show_most_informative_features(32))
-    # tweet = 'I go to school'
-    #print(classifier.classify(Initialize.extract_features(tweet.split())))
+    # print(classifier_pkl.show_most_informative_features(32))
+    # tweet = 'I love to go to school'
+    # print(classifier_pkl.classify(Initialize.extract_features(tweet.split())))
 
     for tweet_split in init_test:
         dist = classifier_pkl.prob_classify(Initialize.extract_features(tweet_split))
         pos = dist.prob("positive")
         neg = dist.prob("negative")
+        print(tweet_split)
 
 
         for label in dist.samples():
              print("%s: %f" % (label, dist.prob(label)))
         # print(pos)
         # sentiment = ''
-        if((pos - neg) > 0.35):
+        if((pos - neg) > 0.3):
              sentiment_list.append("positive")
-        elif((neg - pos) > 0.35):
+        elif((neg - pos) > 0.3):
              sentiment_list.append("negative")
         else:
              sentiment_list.append("neutral")
     # print(sentiment_list)
     dictionary = dict(zip(Initialize.test_tweets_start, sentiment_list))
     return dictionary
-
 
 if __name__ == "__main__": main()
